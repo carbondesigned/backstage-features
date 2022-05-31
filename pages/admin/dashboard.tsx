56c51bce55e6ccf7449a84e65854561f62e8c3f9
Dylan <dylan.reeder5@gmail.com>
2022-05-31 10:51:25 -0400

    better dashboard protection

--- pages/admin/dashboard.tsx
+++ pages/admin/dashboard.tsx
@@ -1,16 +1,29 @@
 import useGetToken from 'hooks/useGetToken';
+import jwtDecode from 'jwt-decode';
 import { useRouter } from 'next/router';
 import React from 'react';
 import { useEffect } from 'react';
 
 type Props = {};
 
+interface Token {
+  email: string;
+  exp: number;
+  id: number;
+}
+
 const Dashboard = (props: Props) => {
   const router = useRouter();
   const token = useGetToken();
 
   useEffect(() => {
-    if (token.length === 0) {
+    let decodedToken: Token;
+    decodedToken = jwtDecode(localStorage.getItem('token') ?? '');
+    if (decodedToken.exp * 1000 < Date.now()) {
+      localStorage.removeItem('token');
+      router.push('/admin/login');
+    }
+    if (token?.email?.length < 0) {
       router.push('/');
     }
   }, []);
