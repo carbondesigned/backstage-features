b888319b48a3462ab49b8def4f89b58496705c43
Dylan <dylan.reeder5@gmail.com>
2022-06-02 12:42:29 -0400

    create post with tags by spliting the data before POST

--- components/FormInput.tsx
+++ components/FormInput.tsx
@@ -1,46 +1,37 @@
-import { DetailedHTMLProps, FC, forwardRef, InputHTMLAttributes } from "react";
-import { Path, RegisterOptions, UseFormRegister } from "react-hook-form";
+import { UseFormRegister } from "react-hook-form";
 
-type InputProps = {
+type Props = {
+  error?: string |  undefined;
+  label: string;
   name: string;
-  label: string
-  type: string
-  placeholder: string
-} & Omit<
-  DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>,
-  'size'
->;
-type FieldProps<T> = {
-  name: Path<T>;
-  register?: UseFormRegister<T>;
-  rules?: RegisterOptions;
-} & Omit<InputProps, 'name'>;
-
-const FieldInput = <T extends Record<string, unknown>>({
-  name,
-  type,
-  register,
-  rules,
-  placeholder,
+  placeholder: string;
+  id?: string;
+  type: string;
+};
+const Input = ({
+  error,
   label,
+  name,
+  placeholder,
+  type,
+  id,
   ...props
-}: FieldProps<T>): JSX.Element => {
-  const inputGroupStyles = `flex flex-col gap-2`;
+}: Props) => {
   return (
-    <div className={inputGroupStyles}>
+    <div className="flex flex-col gap-2">
+      {error && <div>{error}</div>}
       <label htmlFor={name} className="text-xl text-neutral-content">
         {label}
       </label>
       <input
-      {...props}
-        {...(register && register(name, rules))}
+        {...props}
+        id={id}
         type={type}
         name={name}
-        placeholder={placeholder}
         className="input input-md bg-base-200"
       />
     </div>
   );
 };
 
-export default FieldInput
+export default Input;
--- pages/admin/create.tsx
+++ pages/admin/create.tsx
@@ -2,23 +2,27 @@ import DashboardLayout from "components/Layouts/DashbardNav";
 import FieldInput from "components/FormInput";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 import { useMutation, useQueryClient } from "react-query";
 import api from "axiosStore";
 import { useRouter } from "next/router";
+import Input from "components/FormInput";
+import { useState } from "react";
 const CreatePostPage = () => {
+  const [tags, setTags] = useState([""]);
   const router = useRouter();
   const createPostValidation = z.object({
     title: z.string().nullable(),
     excerpt: z.string().nullable(),
     body: z.string().nullable(),
-    tags: z.string().array().nullable(),
+    tags: z.string().nullable(),
   });
   const {
     register,
     handleSubmit,
     watch,
     reset,
+    control,
     formState: { errors },
   } = useForm({
     resolver: zodResolver(createPostValidation),
@@ -26,7 +30,7 @@ const CreatePostPage = () => {
       cover: "",
       title: "",
       excerpt: "",
-      tags: [""],
+      tags: "",
       body: "",
     },
   });
@@ -39,7 +43,7 @@ const CreatePostPage = () => {
       formData.append("title", data.title);
       formData.append("excerpt", data.excerpt);
       formData.append("body", data.body);
-
+      formData.append("tags", data.tags.split(','));
       formData.append("cover", inputFile.files?.item(0) as File);
       return api.post(`/posts/create`, formData, {
         headers: {
@@ -55,9 +59,10 @@ const CreatePostPage = () => {
       },
     }
   );
-
+  errors?.tags && console.log(errors.tags);
   const onSubmit = handleSubmit((data: any) => {
     createPost.mutate(data);
+    
   });
   return (
     <div className="bg-neutral w-full min-h-screen text-base-100">
@@ -80,42 +85,48 @@ const CreatePostPage = () => {
                 className="input input-md bg-base-200"
               />
             </div>
-
-            <div className="flex flex-col gap-2">
-              {errors.title && <div>{errors.title.message}</div>}
-              <label htmlFor="title" className="text-xl text-neutral-content">
-                Title
-              </label>
-              <input
-                {...register("title")}
-                name="title"
-                className="input input-md bg-base-200"
-              />
-            </div>
-            <div className="flex flex-col gap-2">
-              {errors.excerpt && <div>{errors.excerpt.message}</div>}
-              <label htmlFor="excerpt" className="text-xl text-neutral-content">
-                Excerpt
-              </label>
-              <input
-                {...register("excerpt")}
-                name="excerpt"
-                className="input input-md bg-base-200"
-              />
-            </div>
-            <div className="flex flex-col gap-2">
-              {/* {errors.tags && ( */}
-              {/* <div>{errors.tags.message}</div> */}
-              {/* )} */}
-              <label htmlFor="tags" className="text-xl text-neutral-content">
-                Tags
-              </label>
-              <input
-                {...register("tags")}
-                name="tags"
-                className="input input-md bg-base-200"
-              />
-            </div>
+            <Controller
+              control={control}
+              name="title"
+              render={({ field }) => (
+                <Input
+                  {...field}
+                  label="Title"
+                  name="title"
+                  error={errors?.title?.message}
+                  placeholder="title"
+                  type="text"
+                />
+              )}
+            />
+            <Controller
+              name="excerpt"
+              control={control}
+              render={({ field }) => (
+                <Input
+                  {...field}
+                  error={errors?.excerpt?.message}
+                  label="Excerpt"
+                  name="excerpt"
+                  placeholder="Excerpt"
+                  type="text"
+                />
+              )}
+            />
+            <Controller
+              name="tags"
+              control={control}
+              render={({ field }) => (
+                <Input
+                  {...field}
+                  label="Tags"
+                  name="tags"
+                  placeholder="Tags"
+                  type="text"
+                />
+              )}
+            />
+            {/* TODO: work with Marked to handle markdown  */}
             <div className="flex flex-col gap-2">
               {errors.body && <div>{errors.body.message}</div>}
               <label htmlFor="body" className="text-xl text-neutral-content">
