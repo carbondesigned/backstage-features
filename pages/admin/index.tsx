9d2c0b0e92b43cd9a5a6fd5cfb16eafc9ebbd5ae
Dylan <dylan.reeder5@gmail.com>
2022-06-24 08:51:39 -0400

    added proper error for invalid login and adjusted width of form

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import { z } from 'zod';
-import { zodResolver } from '@hookform/resolvers/zod';
-import api from 'axiosStore';
-import { useRouter } from 'next/router';
+import React from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import api from "axiosStore";
+import { useRouter } from "next/router";
 type Props = {};
 
 const AdminHome = (props: Props) => {
+  const [signinErrors, setSigninErrors] = React.useState("");
+
   const router = useRouter();
   const signInValidation = z.object({
     email: z.string().email(),
@@ -21,62 +23,69 @@ const AdminHome = (props: Props) => {
   } = useForm({
     resolver: zodResolver(signInValidation),
     defaultValues: {
-      email: '',
-      password: '',
+      email: "",
+      password: "",
     },
   });
 
   const onSubmit = handleSubmit((data: any) => {
     api
-      .post('/auth/signin', data)
+      .post("/auth/signin", data)
       .then((res) => {
         if (res.status === 200) {
-          localStorage.setItem('token', res.data.token);
-          router.push('/admin/dashboard');
+          localStorage.setItem("token", res.data.token);
+          router.push("/admin/dashboard");
           reset();
         }
       })
       .catch((err) => {
-        console.log(err);
+        console.log(err.response);
+        setSigninErrors(err.response.data.error);
       });
   });
 
   return (
-    <main className='min-h-screen bg-neutral grid place-items-center'>
-      <div className='bg-base-300 p-12 text-base-100 rounded-2xl'>
-        <div className='mx-12'>
-          <h1 className='text-4xl'>Login</h1>
-          <form className='flex flex-col gap-4 mt-12' onSubmit={onSubmit}>
-            <div className='flex flex-col w-full'>
+    <main className="min-h-screen bg-neutral grid place-items-center">
+      <div className="bg-base-300 p-12 text-base-100 rounded-2xl">
+        <div className="w-[25em] mx-auto">
+          <h1 className="text-4xl mb-12">Login</h1>
+          {signinErrors.length > 0 && (
+            <span className="text-error">{signinErrors}</span>
+          )}
+          <form
+            className="flex w-full mx-auto flex-col gap-4"
+            onSubmit={onSubmit}
+          >
+            <div className="flex flex-col w-full">
               {errors.email && (
-                <p className='text-red-500'>{errors.email.message}</p>
+                <p className="text-red-500">{errors.email.message}</p>
               )}
-              <label htmlFor='email' className='text-neutral-content'>
+              <label htmlFor="email" className="text-neutral-content">
                 Email
               </label>
               <input
-                {...register('email')}
-                type='text'
-                placeholder='Enter Email'
-                className='input w-full max-w-xs bg-base-200'
+                {...register("email")}
+                type="text"
+                placeholder="Enter Email"
+                className="input w-full max-w-lg bg-base-200"
               />
             </div>
-            <div className='flex flex-col w-full'>
+            <div className="flex flex-col w-full">
               {errors.password && (
-                <p className='text-red-500'>{errors.password.message}</p>
+                <p className="text-red-500">{errors.password.message}</p>
               )}
-              <label htmlFor='password' className='text-neutral-content'>
+              <label htmlFor="password" className="text-neutral-content">
                 Password
               </label>
               <input
-                {...register('password')}
-                type='password'
-                placeholder='Enter Password'
-                className='input w-full max-w-xs bg-base-200'
+                {...register("password")}
+                type="password"
+                placeholder="Enter Password"
+                className="input w-full max-w-lg bg-base-200"
               />
             </div>
             <div>
-              <button type='submit' className='btn btn-primary w-full'>
+              <button type="submit" className="btn btn-primary w-full">
                 Sign in
               </button>
             </div>
