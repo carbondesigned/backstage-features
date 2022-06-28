a932ce0036fc33f0d64616bdbfa2163f2733262d
Dylan <dylan.reeder5@gmail.com>
2022-06-28 12:38:49 -0400

    fix: svg jsx fix

--- components/Layouts/Navbar.tsx
+++ components/Layouts/Navbar.tsx
@@ -18,9 +18,9 @@ const Navbar = () => {
             className='inline-block w-6 h-6 stroke-base-100'
           >
             <path
-              stroke-linecap='round'
-              stroke-linejoin='round'
-              stroke-width='2'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth='2'
               d='M4 6h16M4 12h16M4 18h16'
             ></path>
           </svg>
