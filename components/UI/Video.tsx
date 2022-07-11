4d606cc693008783ded9e3a008c788dfe8fafca0
Dylan <dylan.reeder5@gmail.com>
2022-07-10 21:22:57 -0400

    fix: too much padding on video component

--- components/UI/Video.tsx
+++ components/UI/Video.tsx
@@ -1,39 +1,39 @@
-import { Line } from "components/Misc/Line"
+import { Line } from "components/Misc/Line";
 
 interface Props {
-  videoId: string
+  videoId: string;
 }
 
 const Video = (props: Props) => {
   return (
     <>
       {props.videoId && (
-        <div className='rounded-xl overflow-hidden relative p-12 border-[2px] post bg-base-200 border-opacity-30 hover:border-opacity-0 border-neutral-content post-border'>
+        <div className="rounded-xl overflow-hidden relative p-4 border-[2px] post bg-base-200 border-opacity-30 hover:border-opacity-0 border-neutral-content post-border">
           {/* brand style around post (lines) */}
-          <div className='line absolute top-0 -left-10 z-50'>
-            <Line line='purple-thick' />
+          <div className="line absolute top-0 -left-10 z-50">
+            <Line line="purple-thick" />
           </div>
-          <div className='line absolute top-48 -right-10'>
-            <Line line='yellow-thick' />
+          <div className="line absolute top-48 -right-10">
+            <Line line="yellow-thick" />
           </div>
-          <div className='line absolute top-24 -right-10'>
-            <Line line='blue-sm' />
+          <div className="line absolute top-24 -right-10">
+            <Line line="blue-sm" />
           </div>
-          <div className='line absolute bottom-10 -left-10 z-0'>
-            <Line line='red-md' />
+          <div className="line absolute bottom-10 -left-10 z-0">
+            <Line line="red-md" />
           </div>
           <iframe
-            className='w-full rounded-xl border-gradient z-40 relative h-full aspect-video'
+            className="w-full rounded-xl border-gradient z-40 relative h-full aspect-video"
             src={`https://www.youtube.com/embed/${props.videoId}`}
-            title='YouTube video player'
-            frameBorder='0'
-            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
           ></iframe>
         </div>
       )}
     </>
-  )
-}
+  );
+};
 
-export default Video
+export default Video;
